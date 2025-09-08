5ea7589d6038782317c2166ebe1534a23c537393
Rahul Dey <rahuldey9623@gmail.com>
2025-09-08 05:25:27 +0000

    Added Express API Setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun ok!");
+import express, { type Request, type Response } from "express";
+
+const APP = express();
+const PORT = 3000;
+
+APP.get("/", (req: Request, res: Response) => {
+  res.json({
+    status: "Success",
+    message: "Welcome to Express API with BUN",
+  });
+});
+
+APP.listen(PORT, () => {
+  console.log(`🚀 Server is running on Port: ${PORT}`);
+});
